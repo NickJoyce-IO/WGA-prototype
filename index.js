db28dd5b3a65b531c697c2c18696962c632f97ec
NickJoyce-IO <brawlyrox@gmail.com>
2020-11-05 14:50:58 +0000

    fix res.send and change static param

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express')
 const app = express()
 
 app.get('/api/test', (req, res) => {
-    res.send('hello', process.env.NODE_ENV)
+    res.send('hello')
 })
 
-if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production')
 {
-    app.use(express.static('client/build'))
+    app.use(express.static('client/dist'))
 
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
     })
 }
 
